8e103f95554cf733336283eb16a022f4775ac05d
Sumit Khandelwal <synergy2411@outlook.com>
2026-09-12 11:47:08 +0530

    generics added

--- ts-basics/src/main.ts
+++ ts-basics/src/main.ts
@@ -148,3 +148,86 @@ tea.execute();
 
 let coffee = new Coffee();
 coffee.execute();
+
+interface User {
+  firstName: string;
+  lastName: string;
+}
+
+let userA: User = {
+  firstName: "John",
+  lastName: "Doe",
+};
+
+// GENERIC FUNCTION
+
+function addAtBeginning<T>(item: T, collection: Array<T>): Array<T> {
+  return [item, ...collection];
+}
+
+let marks = [99, 95, 91, 89, 90];
+const updatedMarks = addAtBeginning<number>(85, marks);
+console.log(typeof updatedMarks[0]); // 'number'
+
+let friends = ["Monica", "Ross", "Joey", "Rachel"];
+const updatedFriends = addAtBeginning<string>("Chandler", friends);
+console.log(typeof updatedFriends[0]); // 'string'
+
+// GENERIC CLASS
+class Stack<T> {
+  private list: T[] = [];
+
+  add(value: T) {
+    this.list.push(value);
+  }
+
+  getAll(): T[] {
+    return this.list.slice(0);
+  }
+
+  remove() {
+    this.list.pop();
+  }
+}
+
+let numberList = new Stack<number>();
+numberList.add(101);
+numberList.add(102);
+numberList.add(103);
+console.log(numberList.getAll());
+numberList.remove();
+console.log(numberList.getAll());
+
+let stringList = new Stack<string>();
+
+let userList = new Stack<User>();
+
+userList.add({ firstName: "Monica", lastName: "Geller" });
+userList.add({ firstName: "Ross", lastName: "Geller" });
+userList.add({ firstName: "Rachel", lastName: "Green" });
+
+console.log(userList.getAll());
+
+// GENERIC INTERFACE
+interface Resource<T, K> {
+  resourceName: T;
+  resourceLocation: K;
+}
+
+let serverOne: Resource<string, number> = {
+  resourceName: "Server One",
+  resourceLocation: 12.34,
+};
+
+let user2 = {
+  firstName: "Monica",
+  lastName: "Geller",
+  age: 21,
+  address: "201, Main Road, Pune",
+};
+
+function demoFn<T extends { firstName: string; lastName: string }>(user: T) {
+  console.log("User : ", user);
+}
+
+demoFn<User>(user2);
