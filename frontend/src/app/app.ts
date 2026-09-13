6fe9c3dfe127881d269ddbe96fec4b3b2ee77eea
Sumit Khandelwal <synergy2411@outlook.com>
2026-09-13 10:57:27 +0530

    model dravenn form added

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,11 +3,12 @@ import { DirectiveDemo } from './core/directive-demo/directive-demo';
 import { PipeDemo } from './core/pipe-demo/pipe-demo';
 import { Users } from './core/users/users';
 import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Users, DirectiveDemo, PipeDemo, Login],
+  imports: [Users, DirectiveDemo, PipeDemo, Login, Register],
 })
 export class App {}
---
+++ frontend/src/app/auth/register/register.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register',
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+  fb = inject(FormBuilder);
+  registerForm: FormGroup;
+
+  constructor() {
+    this.registerForm = this.fb.group({
+      email: new FormControl('test@example.com', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.minLength(6), Validators.required]),
+    });
+  }
+
+  get email() {
+    return this.registerForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.registerForm.get('password') as FormControl;
+  }
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+}
