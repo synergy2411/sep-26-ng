b720bd87bdcccf7375539eb7ed9dd80eca153458
Sumit Khandelwal <synergy2411@outlook.com>
2026-09-13 10:05:40 +0530

    pipe added

--- frontend/src/app/core/pipe-demo/pipe-demo.ts
+++ frontend/src/app/core/pipe-demo/pipe-demo.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pipe-demo',
@@ -15,7 +16,26 @@ export class PipeDemo {
     releaseDate: new Date('Sep 18, 2026'),
   };
 
-  promise = new Promise((resolve) => {
-    setTimeout(() => resolve('Incoming Data from Server'), 1500);
+  promiseTwo: any;
+
+  promiseOne = new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('Promise 1');
+      resolve('1. Incoming Data from Server');
+    }, 1000);
+  }).then(() => {
+    this.promiseTwo = new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('Promise 2');
+        resolve('2. Incoming Data from Server');
+      }, 3000);
+    });
+  });
+
+  obs$ = new Observable((observer) => {
+    setTimeout(() => observer.next('First Package'), 1000);
+    setTimeout(() => observer.next('Second Package'), 2000);
+    setTimeout(() => observer.next('Third Package'), 3000);
+    setTimeout(() => observer.next('Fourth Package'), 4500);
   });
 }
