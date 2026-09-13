e56b67bfebdc90b95240a903493682c9a2b85386
Sumit Khandelwal <synergy2411@outlook.com>
2026-09-13 10:30:50 +0530

    template driven form

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { DirectiveDemo } from './core/directive-demo/directive-demo';
 import { PipeDemo } from './core/pipe-demo/pipe-demo';
 import { Users } from './core/users/users';
+import { Login } from './auth/login/login';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Users, DirectiveDemo, PipeDemo],
+  imports: [Users, DirectiveDemo, PipeDemo, Login],
 })
 export class App {}
